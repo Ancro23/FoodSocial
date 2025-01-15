3837eb407b32af9a182662fa0218b9f9a3a4ce51
Santiago <sanandrespemo@hotmail.com>
2025-01-14 20:52:55 -0500

    clase 14 de enero

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';//IMPORTAMOS EL CUSTOM OARA QUE NOO NOS DE ERROR EL
+
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]// ANADIMOS EL CUSTOM-ELEMT-SCHEMA
 })
 export class AppModule {}
